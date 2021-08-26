09a5a7cda7fd284a86bfd0cfeb43415919b7585c
smotastic <nilssmol@gmail.com>
2021-08-26 14:40:19 +0200

    Simple Backup Script for Cabbagebeyond

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { firestoreExport } = require('node-firestore-import-export');
+const firebase = require('firebase-admin');
+const { firebaseConfig } = require('./lib/config.js');
+const fs = require('fs');
+
+firebase.initializeApp(firebaseConfig);
+
+const backupCollections = ['sw_abilities', 'sw_equipment', 'sw_forces', 'sw_handicaps', 'sw_races', 'sw_talents', 'sw_characters'];
+
+backupCollections.forEach(col => {
+    const collectionRef = firebase.firestore().collection(col);
+
+    firestoreExport(collectionRef)
+        .then(data => fs.writeFile(`backups/${col}.json`, JSON.stringify(data), 'utf8', err => console.log(err)));
+});
+
